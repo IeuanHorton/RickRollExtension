d73fdb7904131451beb91b6f80f2e1e678c52fb1
Ieuan <HortonIeuan@gmail.com>
2020-08-28 23:52:51 -0400

    start

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', onclick, false)
+    
+     function onclick () {
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'blank');  
+    }
+
+}, false)
